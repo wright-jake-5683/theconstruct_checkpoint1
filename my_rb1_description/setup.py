e5efb1d49d96f622d801e3fb1455aea730e6274b
Jake <wrightjake5683@gmail.com>
2025-09-06 23:45:36 +0000

    First commit. Configured package, launch, rviz, and urdf files

---
+++ my_rb1_description/setup.py
@@ -0,0 +1,31 @@
+import os
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = 'my_rb1_description'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.urdf')),
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz')),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.*')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.xacro')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='user',
+    maintainer_email='wrightjake5683@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
