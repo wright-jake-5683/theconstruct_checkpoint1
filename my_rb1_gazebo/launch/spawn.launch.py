cbaf8abe6cc90c4cc1d11a4af49904cd2db6a19a
Jake <wrightjake5683@gmail.com>
2025-09-08 19:10:59 +0000

    Finished second task for checkpoint 1

---
+++ my_rb1_gazebo/launch/spawn.launch.py
@@ -0,0 +1,87 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import (Command, LaunchConfiguration)
+from launch_ros.actions import (Node, SetParameter)
+import launch_ros.descriptions
+
+# ROS2 Launch System will look for this function definition #
+def generate_launch_description():
+
+    # Get Package Description and Directory #
+    package_description = "my_rb1_description"
+    package_directory = get_package_share_directory(package_description)
+
+    # Load URDF File #
+    urdf_file = 'my_rb1_robot.urdf'
+    robot_desc_path = os.path.join(package_directory, "urdf", urdf_file)
+    print("URDF Loaded !")
+
+    # Robot State Publisher (RSP) #
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher_node',
+        output="screen",
+        emulate_tty=True,
+                parameters=[{'use_sim_time': True, 
+                     'robot_description': launch_ros.descriptions.ParameterValue(Command(['xacro ', robot_desc_path]), value_type=str)}]
+    )
+
+    # ROS-Gazebo Bridge #
+    ign_bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        name="ign_bridge",
+        arguments=[
+            "/clock" + "@rosgraph_msgs/msg/Clock" + "[ignition.msgs.Clock",
+            "/cmd_vel" + "@geometry_msgs/msg/Twist" + "@ignition.msgs.Twist",
+            "/tf" + "@tf2_msgs/msg/TFMessage" + "[ignition.msgs.Pose_V",
+            "/odom" + "@nav_msgs/msg/Odometry" + "[ignition.msgs.Odometry",
+            "/laser/scan" + "@sensor_msgs/msg/LaserScan" + "[ignition.msgs.LaserScan",
+        ],
+        remappings=[
+            # there are no remappings for this robot description
+        ],
+        output="screen",
+    )
+
+    # Spawn the Robot #
+    declare_spawn_x = DeclareLaunchArgument("x", default_value="0.0",
+                                            description="Model Spawn X Axis Value")
+    declare_spawn_y = DeclareLaunchArgument("y", default_value="0.0",
+                                            description="Model Spawn Y Axis Value")
+    declare_spawn_z = DeclareLaunchArgument("z", default_value="0.5",
+                                            description="Model Spawn Z Axis Value")
+    declare_spawn_name = DeclareLaunchArgument("model_name", default_value="my_robot",
+                                            description="Model Name Value")                                      
+    gz_spawn_entity = Node(
+        package="ros_gz_sim",
+        executable="create",
+        name="my_robot_spawn",
+        arguments=[
+            "-name", LaunchConfiguration("model_name"),
+            "-allow_renaming", "true",
+            "-topic", "robot_description",
+            "-x", LaunchConfiguration("x"),
+            "-y", LaunchConfiguration("y"),
+            "-z", LaunchConfiguration("z"),
+        ],
+        output="screen",
+    )
+
+    # Create and Return the Launch Description Object #
+    return LaunchDescription(
+        [
+            # Sets use_sim_time for all nodes started below (doesn't work for nodes started from ignition gazebo) #
+            SetParameter(name="use_sim_time", value=True),
+            robot_state_publisher_node,
+            ign_bridge,
+            declare_spawn_x,
+            declare_spawn_y,
+            declare_spawn_z,
+            declare_spawn_name,
+            gz_spawn_entity,
+        ]
+    )
